cf5ae1952c6a139a6d736cac692ff1880a50e876
Jonathon Reading <jonathonreading@gmail.com>
2022-04-22 11:29:41 -1000

    adds secondary function buttons

--- src/calculator/CalculatorComponent.js
+++ src/calculator/CalculatorComponent.js
@@ -9,18 +9,22 @@ export default function CalculatorComponent() {
   const [mode, setMode] = useState('equals');
 
   const handleNum = (num) => {
-    if (prev !== null) {
-      if (result === 0 || result === prev) {
-        setResult(num);
-      } else {
-        setResult(Number(`${result}${num}`));
+    if (result.toString().length === 9) {
+      setResult(result);
+    } else {
+      if (prev !== null) {
+        if (result === 0 || result === prev) {
+          setResult(num);
+        } else {
+          setResult(Number(`${result}${num}`));
+        }
       }
-    }
-    if (prev === null) {
-      if (result === 0) {
-        setResult(num);
-      } else {
-        setResult(Number(`${result}${num}`));
+      if (prev === null) {
+        if (result === 0) {
+          setResult(num);
+        } else {
+          setResult(Number(`${result}${num}`));
+        }
       }
     }
   };
@@ -64,6 +68,20 @@ export default function CalculatorComponent() {
         evaluate();
         setMode('equals');
         break;
+      case 'AC':
+        setMode('equal');
+        setPrev(null);
+        setResult(0);
+        break;
+      case '+-':
+        setMode('equal');
+        setPrev(result);
+        break;
+      case '%':
+        setMode('percent');
+        evaluate();
+        setMode('equals');
+        break;
     }
   };
 
@@ -74,22 +92,35 @@ export default function CalculatorComponent() {
           <h3 className={styles.result}>{result}</h3>
         </div>
         <div className={styles.buttonContainer}>
-          <button type="button" className={styles.numberButton} onClick={() => handleNum(7)}>7</button>
-          <button type="button" className={styles.numberButton} onClick={() => handleNum(8)}>8</button>
-          <button type="button" className={styles.numberButton} onClick={() => handleNum(9)}>9</button>
-          <button type="button" className={styles.primaryFunctionButton} onClick={() => handleFunction('/')}>&#247;</button>
-          <button type="button" className={styles.numberButton} onClick={() => handleNum(4)}>4</button>
-          <button type="button" className={styles.numberButton} onClick={() => handleNum(5)}>5</button>
-          <button type="button" className={styles.numberButton} onClick={() => handleNum(6)}>6</button>
-          <button type="button" className={styles.primaryFunctionButton} onClick={() => handleFunction('x')}>x</button>
-          <button type="button" className={styles.numberButton} onClick={() => handleNum(1)}>1</button>
-          <button type="button" className={styles.numberButton} onClick={() => handleNum(2)}>2</button>
-          <button type="button" className={styles.numberButton} onClick={() => handleNum(3)}>3</button>
-          <button type="button" className={styles.primaryFunctionButton} onClick={() => handleFunction('-')}>-</button>
-          <button type="button" className={styles.numberButton} onClick={() => handleNum(0)}>0</button>
-          <button type="button" className={styles.primaryFunctionButton} onClick={() => handleNum('.')}>.</button>
-          <button type="button" className={styles.primaryFunctionButton} onClick={() => handleFunction('+')}>+</button>
-          <button type="button" className={styles.primaryFunctionButton} onClick={() => handleFunction('=')}>=</button>
+          <div className={styles.buttonRow}>
+            <button type="button" className={styles.secondaryFunctionButton} onClick={() => handleFunction('AC')}>AC</button>
+            <button type="button" className={styles.secondaryFunctionButton} onClick={() => handleFunction('+-')}>+/-</button>
+            <button type="button" className={styles.secondaryFunctionButton} onClick={() => handleFunction('%')}>%</button>
+            <button type="button" className={styles.primaryFunctionButton} onClick={() => handleFunction('/')}>&#247;</button>
+          </div>
+          <div className={styles.buttonRow}>
+            <button type="button" className={styles.numberButton} onClick={() => handleNum(7)}>7</button>
+            <button type="button" className={styles.numberButton} onClick={() => handleNum(8)}>8</button>
+            <button type="button" className={styles.numberButton} onClick={() => handleNum(9)}>9</button>
+            <button type="button" className={styles.primaryFunctionButton} onClick={() => handleFunction('x')}>x</button>
+          </div>
+          <div className={styles.buttonRow}>
+            <button type="button" className={styles.numberButton} onClick={() => handleNum(4)}>4</button>
+            <button type="button" className={styles.numberButton} onClick={() => handleNum(5)}>5</button>
+            <button type="button" className={styles.numberButton} onClick={() => handleNum(6)}>6</button>
+            <button type="button" className={styles.primaryFunctionButton} onClick={() => handleFunction('-')}>-</button>
+          </div>
+          <div className={styles.buttonRow}>
+            <button type="button" className={styles.numberButton} onClick={() => handleNum(1)}>1</button>
+            <button type="button" className={styles.numberButton} onClick={() => handleNum(2)}>2</button>
+            <button type="button" className={styles.numberButton} onClick={() => handleNum(3)}>3</button>
+            <button type="button" className={styles.primaryFunctionButton} onClick={() => handleFunction('+')}>+</button>
+          </div>
+          <div className={styles.buttonRow}>
+            <button type="button" className={styles.zeroButton} onClick={() => handleNum(0)}>0</button>
+            <button type="button" className={styles.primaryFunctionButton} onClick={() => handleNum('.')}>.</button>
+            <button type="button" className={styles.primaryFunctionButton} onClick={() => handleFunction('=')}>=</button>
+          </div>
         </div>
       </div>
     </div>
